39b2d3e57bde1ac25256ddc6c80c7056307620d6
Paddy Mullen <paddy@chartwidget.com>
2010-07-25 17:26:57 -0400

    my first clojure unit tests, starting to flesh out my turtle concept

---
+++ src/logo/turtle.clj
@@ -0,0 +1,49 @@
+(ns logo.turtle
+  (:use
+   [clojure.test]
+   [clojure.contrib.def]
+   ;[rosado.processing.applet]
+   ))
+
+
+(defstruct logo-turtle :position :direction)
+
+(defnk mk-turtle [:position {:x 0 :y 0}
+                  :direction 0]
+  (struct logo-turtle position direction))
+
+(println (mk-turtle))
+
+
+(defn move-point
+  [old-point offset]
+  {
+   :x (+ (old-point :x) (offset :x))
+    :y (+ (old-point :y) (offset :y))})
+
+
+(defn forward
+  [turtle distance]
+  (let
+      [old-point (turtle :positon)]
+    (struct logo-turtle
+            (move-point
+             old-point
+             { :x distance :y 0})
+            :direction (turtle :direction))))
+
+
+(defn clockwise [turtle angle]
+     )
+
+(defn set-direction [turtle angle]
+  (let [corrected-angle (mod angle 360)]
+    (mk-turtle :positon (turtle :position)
+               :direction  corrected-angle)))
+
+
+(deftest test-set-direction
+  (let [t (mk-turtle {:x 100 :y 100}  359)]
+    (is (= 80 (:direction (set-direction t 80))))
+    (is (= 355 (:direction (set-direction t -5))))
+    (is (= 0  (:direction (set-direction t 360))))))
