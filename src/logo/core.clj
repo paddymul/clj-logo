0fbfee6306a3d8941889c7344c8cf5d77040a847
Paddy Mullen <paddy@chartwidget.com>
2010-07-25 16:31:57 -0400

    added a draw-point function,  made the animation stop after 100 frames

--- src/logo/core.clj
+++ src/logo/core.clj
@@ -1,22 +1,23 @@
 (ns logo.core
   (:use
-;   [logo.macrology]
    [rosado.processing]
-   [rosado.processing.applet]))
+   ;[rosado.processing.applet]
+   ))
 
 (defstruct logo-turtle :position :direction)
-
-
-(defn move-point
-  [old-point offset]
-  {
-          :x (+ (old-point :x) (offset :x))
-          :y (+ (old-point :y) (offset :y))})
 
 (defn move-point
   [old-point offset]
   [ (+ (nth old-point 0) (nth offset 0))
     (+ (nth old-point 1) (nth offset 1))])
+
+
+(defn move-point
+  [old-point offset]
+  {
+   :x (+ (old-point :x) (offset :x))
+    :y (+ (old-point :y) (offset :y))})
+
 
 (defn forward
   [turtle distance]
@@ -28,23 +29,31 @@
              { :x distance :y 0})
             :direction (turtle :direction))))
 
-(def pointA (atom [ 50  30]))
+(defn draw-point [a-point]
+  (if (map? a-point)
+    (point (a-point :x) (a-point :y)))
+  (if (vector? a-point)
+    (point (nth a-point 0) (nth a-point 1))))
+
+
+
+(def pointA (atom {:x 50, :y  30}))
 
 (defn move5 [aPoint]
-  (move-point aPoint  [5 7]))
+  (move-point aPoint  {:x 5, :y 7}))
 
-(defn draw
-  []
+(defn draw  []
   (background-float 125)
   (stroke-weight 80)
   (stroke-float 10)
-  (point 100 800)
-  (reset! pointA  (move5 @pointA))
-  ;(println pointA)
-  (point (nth @pointA 0) (nth @pointA 1))
+  (swap!  pointA  move5)
+  (draw-point @pointA)
+
+  (when (> (frame-count) 100)
+    (stop))                             
   )
 
-(defn mouse-move [evt]  (swap! pointA move5))
+
 
 (defn setup []
   (framerate 8)
