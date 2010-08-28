e382b1dbeeea8184f9e7d8cb00f96af13ece7676
Paddy Mullen <paddy@chartwidget.com>
2010-08-28 14:35:16 -0400

    this is now in a different place

--- src/logo/macrology.clj
+++ src/logo/macrology.clj
@@ -8,27 +8,6 @@
 ;; This macro allows us to define an applet as many times as we want
 ;; without having tons of annoying windows all over the desktop
 
-(defmacro re-defapplet [app-name & opts]
-  `(do
-     (try
-       ;; we use eval here because without it, the compiler will catch
-       ;; the fact that app-name isn't defined the first time this is
-       ;; run.  in effect we are using eval to circumvent compiler
-       ;; correctness checking, since this is wrapped in a macro, i'm
-       ;; not too concerned
-       (stop (eval (symbol (str '~app-name))))
-       (catch Exception e#
-         ;(println e#)
-         ;(println "aha you haven't started anything yet")
-         ))
-       (defapplet ~app-name ~@opts)))
-(defmacro rerun-defapplet [app-name & opts]
-  `(do
-     (re-defapplet ~app-name ~@opts)
-
-     (. Thread (sleep 200)) ;sleep here allows stumpwm to catchup 
-     (run ~app-name)))
-
 
 ;(println "")
 ;(pprint (macroexpand-1 `(re-defapplet2 logo-play3 :title "logoemulation")))
